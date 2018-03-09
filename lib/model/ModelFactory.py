42a4d467a86be74351af64d3284ff60454d3d31e
SlipknotTN <micheletoni87@tin.it>
2018-03-09 22:38:24 +0100

    Added DCGAN 64

--- lib/model/ModelFactory.py
+++ lib/model/ModelFactory.py
@@ -1,5 +1,6 @@
 import lib.model.simpleMNIST as simpleMNIST
 import lib.model.dcgan32 as dcgan32
+import lib.model.dcgan64 as dcgan64
 
 
 class ModelFactory(object):
@@ -12,5 +13,7 @@ class ModelFactory(object):
             return simpleMNIST.generator, simpleMNIST.discriminator
         elif config.architecture.lower() == "dcgan_32":
             return dcgan32.generator, dcgan32.discriminator
+        elif config.architecture.lower() == "dcgan_64":
+            return dcgan64.generator, dcgan64.discriminator
         else:
-            raise Exception('Architecture ' + config.model + 'not supported')
+            raise Exception('Architecture ' + config.architecture.lower() + ' not supported')
