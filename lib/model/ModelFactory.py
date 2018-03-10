ef9f80287f99cb8e8f3fc84616bccd1b3f58221d
SlipknotTN <micheletoni87@tin.it>
2018-03-10 14:34:44 +0100

    DCGAN 256 trial

--- lib/model/ModelFactory.py
+++ lib/model/ModelFactory.py
@@ -2,12 +2,16 @@ import lib.model.simpleMNIST as simpleMNIST
 import lib.model.dcgan32 as dcgan32
 import lib.model.dcgan64 as dcgan64
 import lib.model.dcgan128 as dcgan128
+import lib.model.dcgan256 as dcgan256
+import lib.model.wgan64 as wgan64
 
 
 class ModelFactory(object):
 
     @classmethod
     def create(cls, config):
+
+        # TODO: Exploit reflection to save if else lines ...
 
         # Choose model network and build trainable layers
         if config.architecture.lower() == "simple_mnist":
@@ -18,5 +22,9 @@ class ModelFactory(object):
             return dcgan64.generator, dcgan64.discriminator
         elif config.architecture.lower() == "dcgan_128":
             return dcgan128.generator, dcgan128.discriminator
+        elif config.architecture.lower() == "dcgan_256":
+            return dcgan256.generator, dcgan256.discriminator
+        elif config.architecture.lower() == "wgan_64":
+            return wgan64.generator, wgan64.discriminator
         else:
             raise Exception('Architecture ' + config.architecture.lower() + ' not supported')
